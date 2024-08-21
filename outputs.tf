389550fe7eb409c5fa4ecf839a0f95ff0dd6b1c9
indiepopart <jimena@gmail.com>
2024-08-21 20:03:34 -0300

    initial commit

---
+++ outputs.tf
@@ -0,0 +1,16 @@
+output "cluster_id" {
+  value = google_container_cluster.default.id
+}
+
+output "cluster_name" {
+  value = google_container_cluster.default.name
+}
+
+output "cluster_location" {
+  value = google_container_cluster.default.location
+}
+
+output "cluster_endpoint" {
+  value = google_container_cluster.default.endpoint
+}
+
